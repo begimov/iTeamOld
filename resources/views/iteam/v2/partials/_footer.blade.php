f198592706097acc1215ff990b73b54759e250b8
begimov <begimov@gmail.com>
2018-02-02 10:49:56 +0300

    main sections moved to separate partials files

---
+++ resources/views/iteam/v2/partials/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="container-fluid bg-light">
+    <div class="row">
+        <div class="col">
+            FOOTER
+        </div>
+        <div class="col">
+            FOOTER
+        </div>
+        <div class="col">
+            FOOTER
+        </div>
+    </div>
+</div>
