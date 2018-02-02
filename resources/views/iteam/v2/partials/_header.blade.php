6b08ec8aec5755e9f29f4a4e8869ad788620e401
begimov <begimov@gmail.com>
2018-02-02 12:41:41 +0300

    header modified

--- resources/views/iteam/v2/partials/_header.blade.php
+++ resources/views/iteam/v2/partials/_header.blade.php
@@ -1,1 +1,13 @@
-<div>HEADER</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            HEADER
+        </div>
+        <div class="col">
+            HEADER
+        </div>
+        <div class="col">
+            HEADER
+        </div>
+    </div>
+</div>
