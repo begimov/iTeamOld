ac2048882e8e0a704ec87364bc15ec28d91f0d2b
begimov <begimov@gmail.com>
2018-02-02 13:52:03 +0300

    meta tags and scripts added to index template

--- resources/views/iteam/v2/template.blade.php
+++ resources/views/iteam/v2/template.blade.php
@@ -9,6 +9,12 @@
 	<link href="https://iteam.ru/css/style.css?f5=<?=time()?>" rel="stylesheet">
 	<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>iTeam</title>
+	@if(isset($page))
+		<meta name="keywords" content="{{ $page->meta_keywords ? $page->meta_keywords . ' ' : ($page->title ? $page->title . ' ' : '') }}">
+		<meta name="description" content="{{ $page->meta_description ? $page->meta_description . ' ' : ($page->title ? $page->title . ' ' : '') }}">
+		<meta name="og:description" content="{{ $page->meta_description ? $page->meta_description . ' ' : ($page->title ? $page->title . ' ' : '') }}">
+	@endif
+	<meta name="yandex-verification" content="4297394ba43ef620" />
 	@yield('head')
 </head>
 
@@ -23,6 +29,45 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function (d, w, c) {
+			(w[c] = w[c] || []).push(function() {
+				try {
+					w.yaCounter30664892 = new Ya.Metrika({
+						id:30664892,
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true,
+						webvisor:true,
+						trackHash:true
+					});
+				} catch(e) { }
+			});
+			var n = d.getElementsByTagName("script")[0],
+			s = d.createElement("script"),
+			f = function () { n.parentNode.insertBefore(s, n); };
+			s.type = "text/javascript";
+			s.async = true;
+			s.src = "//mc.yandex.ru/metrika/watch.js";
+			if (w.opera == "[object Opera]") {
+				d.addEventListener("DOMContentLoaded", f, false);
+			} else { f(); }
+			})(document, window, "yandex_metrika_callbacks");
+	</script>
+	<noscript><div><img src="//mc.yandex.ru/watch/30664892" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-62052326-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 	@yield('scripts')
 </body>
 </html>
